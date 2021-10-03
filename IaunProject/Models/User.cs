faa2105c5d153c78478941e8ce34fbad5f09df2c
Kourosh <kooroshsajadi@gmail.com>
2021-10-03 19:29:37 +0330

    Refactored the user model to include the other models as nested classes

--- IaunProject/Models/User.cs
+++ IaunProject/Models/User.cs
@@ -10,5 +10,28 @@ namespace IaunProject.Models
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        class PersonalInfo
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Religion { get; set; }
+            public string Occupation { get; set; }
+        }
+        class ContactInfo
+        {
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+        }
+        class Address
+        {
+            [BsonElement("Country")]
+            public string Country { get; set; }
+
+            public string City { get; set; }
+        } 
     }
 }
---
+++ IaunProject/Services/UserService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IaunProject.Models;
+using MongoDB.Driver;
+
+namespace IaunProject.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(IVakilHouseDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _users = database.GetCollection<User>(settings.UsersCollectionName);
+        }
+
+        public List<User> Get()
+        {
+            return _users.Find(user => true).ToList();
+        }
+
+        public User Get(string id)
+        {
+            return _users.Find<User>(user => user.Id == id).FirstOrDefault();
+        }
+
+        public User Create(User book)
+        {
+            _users.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, User bookIn)
+        {
+            _users.ReplaceOne(user => user.Id == id, bookIn);
+        }
+
+        public void Remove(User bookIn)
+        {
+            _users.DeleteOne(user => user.Id == bookIn.Id);
+        }
+
+        public void Remove(string id)
+        {
+            _users.DeleteOne(user => user.Id == id);
+        }
+    }
+}
